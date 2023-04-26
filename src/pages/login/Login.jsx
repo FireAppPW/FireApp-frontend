821ca0060552e99b3ab7600d8fa0b395800150ca
Brafit2001 <100429071@alumnos.uc3m.es>
2023-04-26 12:02:05 +0200

    Login and Register design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./login.scss";
+import screen from "../../assets/images/screen.png";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+      <div className="container">
+        <div className="left">
+          <div className="left-container">
+            <div className="texts">
+              <h2>Welcome to Fire App</h2>
+              <p>Lorem ipsum dolor sit amet </p>
+            </div>
+
+            <form className="form">
+              <div className="formInput">
+                <label htmlFor="">Name</label>
+                <input type="text" />
+                <span className="bar"/>
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Password</label>
+                <input type="password"/>
+                <span className="bar"/>
+              </div>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+        <div className="right">
+          <h2>All in one Dashboard</h2>
+          <img  src={screen} alt="fireSpot"/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+      </div>
+  );
 };
 
 export default Login;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 import "./register.scss";
+import screen from  "../../assets/images/screen.png"
+
 
 const Register = () => {
   return (
     <div className="container">
       <div className="left">
-        <h2>Welcome to Fire App</h2>
-        <form className="form">
-          <div className="formInput">
-            <label htmlFor="">Name</label>
-            <input type="text" placeholder="Name" />
+        <div className="left-container">
+          <div className="texts">
+            <h2>Welcome to Fire App</h2>
+            <p>Lorem ipsum dolor sit amet </p>
           </div>
-          <div className="formInput">
-            <label htmlFor="">Password</label>
-            <input type="password" placeholder="Password" />
-          </div>
-          <button type="submit">Login</button>
-        </form>
+
+          <form className="form">
+            <div className="formInput">
+              <label htmlFor="">Name</label>
+              <input type="text" />
+              <span className="bar"/>
+            </div>
+            <div className="formInput">
+              <label htmlFor="">Username</label>
+              <input type="text" />
+              <span className="bar"/>
+            </div>
+            <div className="formInput">
+              <label htmlFor="">Email</label>
+              <input type="text" />
+              <span className="bar"/>
+            </div>
+            <div className="formInput">
+              <label htmlFor="">Password</label>
+              <input type="password"/>
+              <span className="bar"/>
+            </div>
+            <button type="submit">Register</button>
+          </form>
+        </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <h2>All in one Dashboard</h2>
+        <img  src={screen} alt="fireSpot"/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+      </div>
     </div>
   );
 };
