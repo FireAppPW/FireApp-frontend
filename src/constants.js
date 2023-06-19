0401386b6736aa12ab02e5baf89c0f13724f3418
Minh Nguyen Cong <mcong@box.com>
2023-06-19 23:31:40 +0200

    Update code

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,16 @@
 import Cookies from "js-cookie";
 
-
 //`https://account.fireapp.website/
-export const API_URL = "https://api.fireapp.website"
+export const API_URL = "https://api.fireapp.website";
 
-const token = JSON.parse(Cookies.get('token')).accessToken
+const token = JSON.parse(Cookies.get("token") || "{}").accessToken;
 export const CONFIG = {
-    headers: {
-        Authorization : `Bearer ${token}`
-    }
-}
+	headers: {
+		Authorization: `Bearer ${token}`,
+	},
+};
 
-
-const profileUser = JSON.parse(localStorage.getItem("user"))
-export const PROFILE_DEPARTMENT_ID = profileUser.departmentId
-export const PROFILE_ROLE = profileUser.roles
-export const PROFILE_USER_ID = profileUser.userId
+const profileUser = JSON.parse(localStorage.getItem("user") || "{}");
+export const PROFILE_DEPARTMENT_ID = profileUser.departmentId;
+export const PROFILE_ROLE = profileUser.roles;
+export const PROFILE_USER_ID = profileUser.userId;
